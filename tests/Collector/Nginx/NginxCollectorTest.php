7c2ff1e52f7a1d05d49081fb36b3d0a68485cb27
John <jonathan.plantey@gmail.com>
2026-02-10 22:20:25 +0100

    wip

--- tests/Collector/Nginx/NginxCollectorTest.php
+++ tests/Collector/Nginx/NginxCollectorTest.php
@@ -19,10 +19,46 @@ class NginxCollectorTest extends TestCase
 
     public function testCollect(): void
     {
+        $nginxTOutput = <<<'EOD'
+            nginx: the configuration file /etc/nginx/nginx.conf syntax is ok
+            nginx: configuration file /etc/nginx/nginx.conf test is successful
+
+            # configuration file /etc/nginx/nginx.conf:
+            user www-data;
+            worker_processes auto;
+            pid /run/nginx.pid;
+            include /etc/nginx/modules-enabled/*.conf;
+
+            events {
+                worker_connections 768;
+            }
+
+            http {
+                sendfile on;
+                tcp_nopush on;
+                tcp_nodelay on;
+                keepalive_timeout 65;
+                types_hash_max_size 2048;
+
+                ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+                ssl_prefer_server_ciphers on;
+
+                access_log /var/log/nginx/access.log;
+                error_log /var/log/nginx/error.log warn;
+
+                gzip on;
+
+                include /etc/nginx/conf.d/*.conf;
+                include /etc/nginx/sites-enabled/*;
+            }
+            EOD;
+
         $shellExecutor = $this->createMock(ShellExecutor::class);
         $shellExecutor->method('execute')
-            ->with('nginx -V')
-            ->willReturn("nginx version: nginx/1.22.0\nconfigure arguments: --with-http_ssl_module");
+            ->willReturnMap([
+                ['nginx -V', "nginx version: nginx/1.22.0\nconfigure arguments: --with-http_ssl_module"],
+                ['nginx -T', $nginxTOutput],
+            ]);
 
         $collector = new NginxCollector(__DIR__ . '/_fake_nginx_status.txt', $shellExecutor);
 
@@ -39,6 +75,28 @@ class NginxCollectorTest extends TestCase
         $this->assertSame(6, $metrics['status']['reading']);
         $this->assertSame(179, $metrics['status']['writing']);
         $this->assertSame(106, $metrics['status']['waiting']);
+
+        $this->assertIsArray($metrics['config']);
+        $this->assertSame('/etc/nginx/nginx.conf', $metrics['config']['config_path']);
+        $this->assertSame('www-data', $metrics['config']['user']);
+        $this->assertSame('auto', $metrics['config']['worker_processes']);
+        $this->assertSame('768', $metrics['config']['worker_connections']);
+        $this->assertSame('on', $metrics['config']['sendfile']);
+        $this->assertSame('on', $metrics['config']['tcp_nopush']);
+        $this->assertSame('on', $metrics['config']['tcp_nodelay']);
+        $this->assertSame('65', $metrics['config']['keepalive_timeout']);
+        $this->assertSame('2048', $metrics['config']['types_hash_max_size']);
+        $this->assertNull($metrics['config']['server_tokens']);
+        $this->assertSame('TLSv1 TLSv1.1 TLSv1.2 TLSv1.3', $metrics['config']['ssl_protocols']);
+        $this->assertSame('on', $metrics['config']['ssl_prefer_server_ciphers']);
+        $this->assertSame('/var/log/nginx/access.log', $metrics['config']['access_log']);
+        $this->assertSame('/var/log/nginx/error.log warn', $metrics['config']['error_log']);
+        $this->assertSame('on', $metrics['config']['gzip']);
+        $this->assertSame([
+            '/etc/nginx/modules-enabled/*.conf',
+            '/etc/nginx/conf.d/*.conf',
+            '/etc/nginx/sites-enabled/*',
+        ], $metrics['config']['include']);
     }
 
     public function testGetName(): void
