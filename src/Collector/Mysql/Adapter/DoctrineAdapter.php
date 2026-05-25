c054f9c8249d56acd8bccf0633174603e0fa9a58
John <jonathan.plantey@gmail.com>
2026-05-25 18:13:50 +0200

    Deprecate old mysql adapters

---
+++ src/Collector/Mysql/Adapter/DoctrineAdapter.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jmonitor\Collector\Mysql\Adapter;
+
+use Jmonitor\Utils\DatabaseAdapter\DoctrineAdapter as BaseDoctrineAdapter;
+
+/**
+ * @deprecated Use \Jmonitor\Utils\DatabaseAdapter\DoctrineAdapter instead.
+ */
+class DoctrineAdapter extends BaseDoctrineAdapter
+{
+}
